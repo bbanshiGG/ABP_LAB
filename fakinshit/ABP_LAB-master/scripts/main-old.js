d5682a6441409e8e521a2edd11003bb1c91b16ed
bbanshi <bbanshigg@gmail.com>
2023-10-22 00:38:17 -0300

    MEU DEUS CARALHO

--- fakinshit/ABP_LAB-master/scripts/main-old.js
+++
@@ -1,8 +0,0 @@
-let listElements = ducument.querySelectorAll('.list__button--click');
-
-listElements.forEach(listElements =>{
-    listElements.addEventListener('click',()=>{
-        
-        listElements.classList.toggle('arrow');
-    })
-});
